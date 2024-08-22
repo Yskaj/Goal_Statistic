ed774b31769ed2b4c596265806b8f6c18c022891
Ksilen <yskaj@yandex.ru>
2024-08-22 13:06:35 +0400

    add goalscorers.csv to main

--- src/main/java/org/statistic_goal/Main.java
+++ src/main/java/org/statistic_goal/Main.java
@@ -5,8 +5,9 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        Path csv =  Paths.get("src", "test", "java","tests","test.csv");
+    public static void main(String[] args){
+        //Path csv =  Paths.get("src", "test", "java","tests","test.csv");
+        Path csv =  Paths.get("src", "test", "java","goalscorers.csv");
         GoalStatistics.getGoalStatistics(csv.toString());
     }
 }
