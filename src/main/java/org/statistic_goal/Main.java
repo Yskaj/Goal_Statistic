6e1281ea96895c69a219183fe89b3f7b69d2a7e6
Ksilen <yskaj@yandex.ru>
2024-09-16 02:49:31 +0400

    Stream Method

--- src/main/java/org/statistic_goal/Main.java
+++ src/main/java/org/statistic_goal/Main.java
@@ -1,13 +1,13 @@
 package org.statistic_goal;
 
-import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class Main {
-    public static void main(String[] args){
-        //Path csv =  Paths.get("src", "test", "java","tests","test.csv");
-        Path csv =  Paths.get("src", "test", "java","goalscorers.csv");
-        GoalStatistics.getGoalStatistics(csv.toString());
+    public static void main(String[] args) {
+        Path path = Paths.get("src", "test", "java", "goalscorers.csv");
+//        Converter converter = new ConvertCVStoJSON(path);
+//        converter.convert();
+        StreamCVStoJSON json = new StreamCVStoJSON(path);
     }
 }
